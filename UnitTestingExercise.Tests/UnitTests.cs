b909887f7112931fe8930c4bb7502f42963a8cc0
msund385 <ms43004foru@gmail.com>
2020-10-21 23:32:12 -0400

    init commit

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -1,77 +1,94 @@
 using System;
 using Xunit;
+using Xunit.Sdk;
 
 namespace UnitTestingExercise.Tests
 {
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1, 2, 3, 6)]
+        [InlineData(5, 5, 5, 15)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.Add(num1, num2, num3);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(5, 5, 0)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
-
+            var tester = new UnitTestMethods();
             //Act
-
+            var actual = tester.Subtract(minuend, subtrhend);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(2, 2, 4)]
+        [InlineData(5, 4, 20)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
-
+            var tester = new UnitTestMethods();
             //Act
-
+            var actual = tester.Multiply(num1, num2);
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(20, 5, 4)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.Divide(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
-        [Fact]
-        public void YourMethodName()
+        [Theory]
+        [InlineData(new int[] { 5, 4, 10, 50, 99 }, 99)]
+        public void GetMax(int[] numbers, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.GetMax(numbers);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
-        [Fact]
-        public void YourMethodName2()
-        {
+
+        [Theory]
+        [InlineData(new int[] { 2, 5, 8, 9}, 2)]
+           public void GetMin(int[] numbers, int expected)
+           {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.GetMin(numbers);
             //Assert
-        }
+            Assert.Equal(expected, actual);
+
+           }
+         
     }
-}
+}       
+
+       
+
+            
+            
+    
+
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -1,26 +1,49 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Linq;
+
 namespace UnitTestingExercise
 {
     public class UnitTestMethods
     {
-        // Make sure your references are set properly
+        // Done!Make sure your references are set properly
             // Righ-click on Dependencies in your .Tests project and set your reference
 
 
 
         // Create an Add method that passes 3 integers
-
+        public int Add(int one, int two, int three)
+        {
+            return one + two + three;
+        }
         // Create a Subtract method that passes 2 integers
             // Keep track of which number is getting passed as minuend and subtrahend
-
+            public int Subtract(int minuend, int subtrhend)
+            {
+              return minuend - subtrhend;
+            }
         // Create a Multiply method that passes 2 integers
-
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }       
         // Create a Divide method that passes 2 integers
+        public int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
 
 
-
-        // Create 2 methods that will utilize the [Fact] tests you wrote
-
+        
+        public int GetMax(int[] numbers)
+        {
+            return numbers.Max();
+                    
+        }
+        public int GetMin(int[] numbers)
+        {
+            return numbers.Min();
+        }
         
     }
 }
